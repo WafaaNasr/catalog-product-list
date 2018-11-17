d3444ed6124b8719e5b00b5edb8e9fe26cda7857
Wafaa <Wa@airsupport.dk>
2018-11-17 15:55:20 +0200

    define product properties

--- src/app/product/models/product.ts
+++ src/app/product/models/product.ts
@@ -1,3 +1,11 @@
 export class Product {
-
+    id: string;
+    name: string;
+    slug: string;
+    brand: string;
+    type: string;
+    image: string;
+    price: number;
+    size: string;
+    rating: number;
 }
